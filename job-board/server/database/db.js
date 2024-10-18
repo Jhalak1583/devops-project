8ebccb773249231c22877b94de4b141f4c37faa2
Jhalak1583 <vaghasiya.jhalak15@gmail.com>
2024-10-18 16:30:13 +0530

    Removed unnecessary files and updated structure

--- job-board/server/database/db.js
+++ job-board/server/database/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 const DBConnection = async () => {
-
-    const MONGO_URI = `mongodb+srv://user:codeforinterview@job-portal.ri6pbum.mongodb.net/`;
-    try {
-        await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log('Error while connecting with the database ', error.message);
-    }
-}
+  const MONGO_URI = `mongodb+srv://jhalak:1234@cluster0.lai3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  try {
+    await mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error while connecting with the database ", error.message);
+  }
+};
 
 export default DBConnection;
